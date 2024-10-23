657bc8e69ae1e9b9413a572985ac8b179ca88c52
Alexandre Guihard <guihardalex98@gmail.com>
2024-10-23 10:45:04 +0200

    Modèle: Participants

---
+++ src/modele/ParticipantNonLicenceCourseIndiv.java
@@ -0,0 +1,16 @@
+public class ParticipantNonLicenceCourseIndiv extends Participant{
+    private String dateDeNaissance;
+
+    public ParticipantNonLicenceCourseIndiv(int id, String nom, String prenom, String categorie, char sexe, String email, String ville, String certification, int numTel, String dateDeNaissance){
+        super(id, nom, prenom, categorie, sexe, email, ville, certification, numTel);
+        this.dateDeNaissance=dateDeNaissance;
+    }
+
+    public String getDateDeNaissance(){
+        return this.dateDeNaissance;
+    }
+
+    public void setDateDeNaissance(String dateDeNaissance){
+        this.dateDeNaissance=dateDeNaissance;
+    }
+}
