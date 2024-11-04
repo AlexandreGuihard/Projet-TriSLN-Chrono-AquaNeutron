46f40b43f5ea839b9e1035e7b8be1ab7c9e994c9
Amine-Yah <mohamed-amine.yahyaoui@etu.univ-orleans.fr>
2024-11-04 15:44:25 +0100

    modification de Classement

--- src/modele/Classement.java
+++ src/modele/Classement.java
@@ -1,27 +1,34 @@
-package modele;
+package src.modele;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import modele.Participant;
+import modele.Exceptions.WrongInformationException;
 import modele.Participant;
 
-public class Classement {
-    private List<Participant> lesParticipants;
+public class Classement{
     private int id;
+    private int posGeneral;
     private String posCategorie;
     private int posClub;
+    private String temps;
+    private Participant participant;
 
-    public Classement(int id, String posCategorie, int posClub){
+    public Classement(int id, int posGeneral, String posCategorie, int posClub, String temps){ 
         this.id = id;
+        this.posGeneral = posGeneral;
         this.posCategorie = posCategorie;
         this.posClub = posClub;
-        this.lesParticipants = new ArrayList<Participant>();
-    }
-
-    public List<Participant> getLesParticipants() {
-        return lesParticipants;
+        this.temps = temps;
     }
 
     public int getId() {
         return id;
+    }
+
+    public int getPosGeneral() {
+        return posGeneral;
     }
 
     public String getPosCategorie() {
@@ -32,8 +39,20 @@ public class Classement {
         return posClub;
     }
 
+    public String getTemps() {
+        return temps;
+    }
+
+    public Participant getParticipant() {
+        return participant;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void setPosGeneral(int posGeneral) {
+        this.posGeneral = posGeneral;
     }
 
     public void setPosCategorie(String posCategorie) {
@@ -43,8 +62,10 @@ public class Classement {
     public void setPosClub(int posClub) {
         this.posClub = posClub;
     }
-
-    public void addParticipant(Participant participant) {
-        this.lesParticipants.add(participant);
+    public void setTemps(String temps) {
+        this.temps = temps;
+    }
+    public void setParticipant(Participant participant) {
+        this.participant = participant;
     }
 }
