d8b1d1931fad26d2f260d1f0942c3b640f7903d3
Toniomp <delahayetonio77@gmail.com>
2024-11-04 14:38:29 +0100

    méthodes connecter et afficheLogin

--- src/vue/FenetreLogin.java
+++ src/vue/FenetreLogin.java
@@ -1,20 +1,31 @@
+package src.vue;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import src.vue.*;
 
 
 public class FenetreLogin {
 
-
+    private BorderPane root;
     private TextField identifier;
     private PasswordField mdp;
     private Button bConnexion;     
 
-    public FenetreLogin(){
-
+    public FenetreLogin(FXMLLoader loader){
+        this.root = new BorderPane();
         this.identifier = new TextField();       
         this.mdp = new PasswordField();
         this.bConnexion = new Button();
+        loader.setController(new ControleurBoutons(new TriSLN()));
+        this.afficheLogin(loader);
     }
 
     public String getIdentifiant() {
@@ -41,7 +52,50 @@ public class FenetreLogin {
         this.bConnexion = bouton;
     }
 
+    public BorderPane getRoot(){
+        return this.root;
+    }
+
+
     public void connecter() {
-        
+        String identifiant = getIdentifiant();
+        String motDePasse = getMotDePasse();
+        BdTriSLN bd = TriSLN.getBd();
+        if (bd.verifConnexion(identifiant, motDePasse))
+        {
+            try {
+                Stage StageVue = new Stage();
+                this.root = FXMLLoader.load(getClass().getResource("SAEprojetAccueilConnecter.fxml"));
+                Scene scene = new Scene(this.root);
+                StageVue.setScene(scene);
+                StageVue.show();
+            } 
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            this.mdp.setText("");
+            Text t = new Text("Identifiant ou Mot de passe incorrect.");
+            t.setFill(Color.RED);
+            this.root.getChildren().add(t);
+
+        }
+    }
+
+    public void afficheLogin(FXMLLoader loader){
+        try {
+            Stage StageVue = new Stage();
+            this.root =  (BorderPane) loader.load();
+            Scene scene = new Scene(this.root);
+            StageVue.setScene(scene);
+            StageVue.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
