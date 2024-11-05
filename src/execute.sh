2ae3b3550604d93f674a7741bef01440a43385af
Alexandre Guihard <guihardalex98@gmail.com>
2024-11-05 14:03:05 +0100

    Déplacement du script de lancement

--- src/execute.sh
+++
@@ -1,3 +0,0 @@
-clear
-javac -d bin --module-path /usr/share/openjfx/lib:/usr/share/java/mariadb-java-client.jar --add-modules javafx.controls,javafx.fxml src/vue/*.java
-java -cp bin --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml src.vue.TriSLN
