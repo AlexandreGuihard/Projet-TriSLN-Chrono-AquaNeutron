9834cb780a0dc944a613ddd7398bdcf8af7141d3
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-11-06 08:57:12 +0100

    Ajout role dans base de données

---
+++ src/bd/role.sql
@@ -0,0 +1,17 @@
+create role administrateur, spectateur, benevol;
+
+create user admin identified by adminMdp;
+create user invite identified by nonosquare;
+
+
+
+grant all on DBguihard.* to administrateur;
+
+grant select on DBguihard.* to benevol;
+grant insert on DBguihard.COURSES to benevol;
+grant insert on DBguihard.CLASSEMENT to benevol;
+
+grant select on DBguihard.CLASSEMENT to spectateur;
+
+grant administrateur to admin;
+grant spectateur to invite;
