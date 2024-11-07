f578176c704fbdc5d34f4609cfe113014c2fdb07
renaudin clement <cronisosdev@gmail.com>
2024-11-07 12:17:06 +0100

    mise en place pour insert

--- src/bd/BD.sql
+++ src/bd/BD.sql
@@ -6,34 +6,32 @@ drop TABLE CHRONOMETRAGE;
 drop TABLE DOSSARD;
 drop TABLE PARTICIPANT;
 drop TABLE EPREUVE;
-
-
 drop TABLE UTILISATEUR;
 
 CREATE TABLE CHRONOMETRAGE (
     id_Chrono     INT NOT NULL,
-    temps_depart TIME,
+    temps_depart  TIME,
     temps_arrivee TIME,
     PRIMARY KEY (id_Chrono)
 );
 
 CREATE TABLE CLASSEMENT (
     id_Classement INT NOT NULL,
-    pos_generale INT,
+    pos_generale  INT,
     pos_categorie INT,
-    pos_club INT,
-    temps TIME,
+    pos_club      INT,
+    temps         TIME,
     PRIMARY KEY (id_Classement)
 );
 
 CREATE TABLE DOSSARD (
-    num_dossard INT,
+    num_dossard    INT,
     id_Participant INT,
     PRIMARY KEY (num_Dossard)
 );
 
 CREATE TABLE ENREGISTRER (
-    id_Chrono INT,
+    id_Chrono   INT,
     num_Dossard INT,
     PRIMARY KEY (id_Chrono, num_Dossard)
 );
@@ -49,8 +47,8 @@ CREATE TABLE EPREUVE (
 );
 
 CREATE TABLE GENERER (
-    id_Classement INT,
-    id_Epreuve INT,
+    id_Classement  INT,
+    id_Epreuve     INT,
     id_Participant INT,
     PRIMARY KEY (id_Classement, id_Epreuve, id_Participant)
 );
@@ -76,7 +74,7 @@ CREATE TABLE PARTICIPANT (
 CREATE TABLE PARTICIPER (
     id_Participant INT NOT NULL,
     id_Epreuve     INT NOT NULL,
-    payee          VARCHAR(42),
+    payee          BOOLEAN,
     PRIMARY KEY (id_Participant, id_Epreuve)
 );
 
@@ -86,13 +84,15 @@ CREATE TABLE UTILISATEUR (
     PRIMARY KEY (identifiant)
 );
 
-ALTER TABLE DOSSARD ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
+ALTER TABLE PARTICIPER  ADD FOREIGN KEY (id_Epreuve)     REFERENCES EPREUVE (id_Epreuve);
+ALTER TABLE PARTICIPER  ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
 
-ALTER TABLE ENREGISTRER ADD FOREIGN KEY (num_Dossard) REFERENCES DOSSARD (num_Dossard);
-ALTER TABLE ENREGISTRER ADD FOREIGN KEY (id_Chrono) REFERENCES CHRONOMETRAGE (id_Chrono);
-ALTER TABLE GENERER ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
-ALTER TABLE GENERER ADD FOREIGN KEY (id_Epreuve) REFERENCES EPREUVE (id_Epreuve);
-ALTER TABLE GENERER ADD FOREIGN KEY (id_Classement) REFERENCES CLASSEMENT (id_Classement);
+ALTER TABLE GENERER     ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
+ALTER TABLE GENERER     ADD FOREIGN KEY (id_Epreuve)     REFERENCES EPREUVE (id_Epreuve);
+ALTER TABLE GENERER     ADD FOREIGN KEY (id_Classement)  REFERENCES CLASSEMENT (id_Classement);
 
-ALTER TABLE PARTICIPER ADD FOREIGN KEY (id_Epreuve) REFERENCES EPREUVE (id_Epreuve);
-ALTER TABLE PARTICIPER ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
+
+ALTER TABLE DOSSARD     ADD FOREIGN KEY (id_Participant) REFERENCES PARTICIPANT (id_Participant);
+
+ALTER TABLE ENREGISTRER ADD FOREIGN KEY (num_Dossard)    REFERENCES DOSSARD (num_Dossard);
+ALTER TABLE ENREGISTRER ADD FOREIGN KEY (id_Chrono)      REFERENCES CHRONOMETRAGE (id_Chrono);
--- src/bd/inserte.sql
+++ src/bd/inserte.sql
@@ -1,18 +1,27 @@
+INSERT INTO PARTICIPER (INT id_Participant,INT id_Epreuve,BOOLEAN payee) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO CHRONOMETRAGE (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO GENERER (INT id_Classement,INT id_Epreuve,INT id_Participant,) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO CLASSEMENT (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO CLASSEMENT (INT id_Classement,INT pos_generale,INT pos_categorie,INT pos_club,INT temps) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO DOSSARD (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO ENREGISTRER (id_Chrono   INT,num_Dossard INT,) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO ENREGISTRER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO CHRONOMETRAGE (INT id_Chrono ,TIME temps_depart,TIME temps_arrivee) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO EPREUVE (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO DOSSARD (INT num_dossard,INT id_Participant) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO GENERER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO EPREUVE (INT id_Epreuve,VARCHAR(42) nom_Epreuve,VARCHAR(42) format,BOOLEAN categorie,TIME heure_Depart,INT prix,) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO PARTICIPANT (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO PARTICIPANT (INT id_Participant,VARCHAR(42) nom,VARCHAR(42) prenom,VARCHAR(42) categorie,VARCHAR(42) sexe,VARCHAR(42) email,VARCHAR(42) ville,BOOLEAN certification,INT num_Tel,VARCHAR(42) club,INT num_Licence,DATE date_Naissance,VARCHAR(42) nom_Equipe,BOOLEAN licence,) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO PARTICIPER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+INSERT INTO UTILISATEUR ( VARCHAR(42) identifiant,VARCHAR(42) mot_de_passe) VALUES 
+(value1, value2, value3, ...); 
 
-INSERT INTO UTILISATEUR (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
