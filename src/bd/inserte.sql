4fb25305ba43052a732cb0946323e9672d57ca94
renaudin clement <cronisosdev@gmail.com>
2024-11-07 10:56:50 +0100

    mise en place de drop table

---
+++ src/bd/inserte.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO CHRONOMETRAGE (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO CLASSEMENT (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO DOSSARD (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO ENREGISTRER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO EPREUVE (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO GENERER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO PARTICIPANT (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO PARTICIPER (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+
+INSERT INTO UTILISATEUR (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
