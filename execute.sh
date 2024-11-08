4f45f5a4bae5db88c55c987a57cc80eaf8960fec
renaudin clement <pro.renaudin.clement@gmail.com>
2024-11-08 11:17:35 +0100

    Delete execute.sh

--- execute.sh
+++
@@ -1,3 +0,0 @@
-clear
-javac -d bin --module-path /usr/share/openjfx/lib:/usr/share/java/mariadb-java-client.jar --add-modules javafx.controls,javafx.fxml src/vue/*.java
-java -cp bin:/usr/share/java/mariadb-java-client.jar --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml src.vue.TriSLN
