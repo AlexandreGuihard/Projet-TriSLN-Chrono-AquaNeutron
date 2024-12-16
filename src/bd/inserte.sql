0b2aa32fec1c1649e0c827597fdb9bdb2c5270e7
renaudin clement <cronisosdev@gmail.com>
2024-12-16 08:54:34 +0100

    version de la semain derniere

--- src/bd/inserte.sql
+++ src/bd/inserte.sql
@@ -33,12 +33,12 @@ INSERT INTO EPREUVE (INT id_Epreuve,VARCHAR(42) nom_Epreuve,VARCHAR(42) format,B
 (value1, value2, value3, ...); 
 
 INSERT INTO PARTICIPANT (INT id_Participant,VARCHAR(42) nom,VARCHAR(42) prenom,VARCHAR(42) categorie,VARCHAR(42) sexe,VARCHAR(42) email,VARCHAR(42) ville,BOOLEAN certification,INT num_Tel,VARCHAR(42) club,INT num_Licence,DATE date_Naissance,VARCHAR(42) nom_Equipe,BOOLEAN licence,) VALUES 
-(1, "Dupont", "Xavier", "MP","F","x.dupont@yahoo.fr","Paris",TRUE,0647882565,"SQL",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"bleu",TRUE)
+(1, "Dupont", "carle", "MP","F","x.carle@yahoo.fr","Paris",TRUE,0647882565,"SQL",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"bleu",TRUE)
 
 (2, "Pilaf", "mila", "PO","F","mila.Pilaf@yahoo.fr","Orleans",TRUE,0692243352,"SQL",1,TO_DATE('24-MARS-2001','DD-MON-YYYY'),"bleu",FALSE)
 (3, "Sparkis", "devoid", "S","M","devoid.Sparkis@yahoo.fr","Angers",FALSE,0695476132,"Marchal",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"jaune",TRUE)
 (4, "Daniel", "Antoine", "MP","F","x.dupont@yahoo.fr","Paris",TRUE,0647882565,"SQL",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"rouge",TRUE)
-(5, "Dupont", "Xavier", "MP","F","x.dupont@yahoo.fr","Paris",TRUE,0647882565,"SQL",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"noire",TRUE)
+(5, "Dupont", "dupon", "MP","F","x.dupon@yahoo.fr","Paris",TRUE,0647882565,"SQL",1,TO_DATE('15-JUN-2001','DD-MON-YYYY'),"noire",TRUE)
 ; 
 
 -- numero qui marche 0692243352
